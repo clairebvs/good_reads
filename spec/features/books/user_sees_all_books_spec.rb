dd6034ff41e385cb59557ff1a39554ff30da0a1c
ClaireBvs <clairembeauvais@gmail.com>
2018-07-13 10:06:08 -0600

    Set up bundle and add routes

---
+++ spec/features/books/user_sees_all_books_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'a user visit the index page' do
+  describe 'user sees all books and titles' do
+    it 'can click on a book title so go to the book show page' do
+
+    end 
+  end
+end
