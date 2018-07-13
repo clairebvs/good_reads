6757e23c62b7bb1326199109d3ebc30c6a639c60
ClaireBvs <clairembeauvais@gmail.com>
2018-07-13 10:50:58 -0600

    add second user story

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20180713162332) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "review"
+    t.string "rating"
   end
 
   add_foreign_key "user_books", "books"
---
+++ spec/features/books/user_sees_show_page_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'a user visit the show page book' do
+  it 'user sees book title and review and name of user' do
+      book1 = Book.create(title: "1, 2, 3")
+      book2 = Book.create(title: "Brooklyn Bridge")
+      user1 = User.create(name: "Elena", review: "Fantastic", rating: 5)
+      user2 = User.create(name: "John", review: "Difficult start", rating: 2)
+      UserBook.create(book_id: book1.id, user_id: user1.id)
+      UserBook.create(book_id: book1.id, user_id: user2.id)
+
+      visit book_path(book1)
+
+      expect(page).to have_content(book1.title)
+      expect(page).to have_content(user1.review)
+      expect(page).to have_content(user1.name)
+      expect(page).to have_content(user2.review)
+      expect(page).to have_content(user2.name)
+    end
+  end
