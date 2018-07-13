50182b7a81e8d6d2ef4f525b58848f8fba595d86
ClaireBvs <clairembeauvais@gmail.com>
2018-07-13 10:24:30 -0600

    add column to user table and add join table user_books

---
+++ db/migrate/20180713162332_create_user_books.rb
@@ -0,0 +1,8 @@
+class CreateUserBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :user_books do |t|
+      t.references :book, foreign_key: true
+      t.references :user, foreign_key: true
+    end
+  end
+end
