54186e247f5d5f1c1e81c1e2612dc6d78e21efb9
ClaireBvs <clairembeauvais@gmail.com>
2018-07-13 10:17:51 -0600

    Add first user story

--- spec/features/books/user_sees_all_books_spec.rb
+++ spec/features/books/user_sees_all_books_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
 describe 'a user visit the index page' do
-  describe 'user sees all books and titles' do
-    it 'can click on a book title so go to the book show page' do
+  it 'user sees all books and titles' do
+      book1 = Book.create(title: "1, 2, 3")
+      book2 = Book.create(title: "Brooklyn Bridge")
 
-    end 
+      visit books_path
+
+      expect(page).to have_content(book1.title)
+      expect(page).to have_content(book2.title)
+    end
+
+    it 'can click on a book title and go to the book show page' do
+      book1 = Book.create(title: "1, 2, 3")
+      book2 = Book.create(title: "Brooklyn Bridge")
+
+      visit books_path
+
+      click_link ("#{book1.title}")
+
+      expect(current_path).to eq(book_path(book1))
   end
 end
