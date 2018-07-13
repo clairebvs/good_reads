606503b71fdf3e3bec2c779f851759c2c029ff2f
ClaireBvs <clairembeauvais@gmail.com>
2018-07-13 11:01:16 -0600

    wip on rating

--- spec/features/books/user_sees_show_page_spec.rb
+++ spec/features/books/user_sees_show_page_spec.rb
@@ -17,4 +17,18 @@ describe 'a user visit the show page book' do
       expect(page).to have_content(user2.review)
       expect(page).to have_content(user2.name)
     end
+
+    it 'user can see average rating' do
+      book1 = Book.create(title: "1, 2, 3")
+      user1 = User.create(name: "Elena", review: "Fantastic", rating: 5)
+      user2 = User.create(name: "John", review: "Difficult start", rating: 2)
+      UserBook.create(book_id: book1.id, user_id: user1.id)
+      UserBook.create(book_id: book1.id, user_id: user2.id)
+
+      visit book_path(book1)
+
+      expected_result = 3.5
+
+      expect(page).to have_content(expected_result)
+    end
   end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,4 +5,18 @@ describe Book, type: :model do
     it {should have_many(:user_books)}
     it {should have_many(:users).through(:user_books)}
   end
+
+  describe 'instance method' do
+    it 'should calculate average rating per book' do
+      book1 = Book.create(title: "1, 2, 3")
+      user1 = User.create(name: "Elena", review: "Fantastic", rating: 5)
+      user2 = User.create(name: "John", review: "Difficult start", rating: 2)
+      UserBook.create(book_id: book1.id, user_id: user1.id)
+      UserBook.create(book_id: book1.id, user_id: user2.id)
+
+      expected_result = 3.5
+
+      expect(book1.average_rating).to eq(expected_result)
+    end
+  end
 end
