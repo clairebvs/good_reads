57da9318972513e6c24487b1af4f495c6557c513
ClaireBvs <clairembeauvais@gmail.com>
2018-07-15 15:29:05 -0600

    Add table reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180713162332) do
+ActiveRecord::Schema.define(version: 20180715212745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,19 +19,19 @@ ActiveRecord::Schema.define(version: 20180713162332) do
     t.string "title"
   end
 
-  create_table "user_books", force: :cascade do |t|
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "rating"
     t.bigint "book_id"
     t.bigint "user_id"
-    t.index ["book_id"], name: "index_user_books_on_book_id"
-    t.index ["user_id"], name: "index_user_books_on_user_id"
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "review"
-    t.string "rating"
   end
 
-  add_foreign_key "user_books", "books"
-  add_foreign_key "user_books", "users"
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
