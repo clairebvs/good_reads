11838094245cb1d447eafaf8b6bd9136a2446642
ClaireBvs <clairembeauvais@gmail.com>
2018-07-15 16:45:42 -0600

    Add average rating instance method and added it to book show page

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -9,10 +9,11 @@ describe Book, type: :model do
   describe 'instance method' do
     it 'should calculate average rating per book' do
       book1 = Book.create(title: "1, 2, 3")
-      user1 = User.create(name: "Elena", review: "Fantastic", rating: 5)
-      user2 = User.create(name: "John", review: "Difficult start", rating: 2)
-      UserBook.create(book_id: book1.id, user_id: user1.id)
-      UserBook.create(book_id: book1.id, user_id: user2.id)
+      book2 = Book.create(title: "Brooklyn Bridge")
+      user1 = User.create(name: "Elena")
+      user2 = User.create(name: "John")
+      review1 = Review.create(book_id: book1.id, user_id: user1.id, comment: "Great book", rating: 5)
+      review2 = Review.create(book_id: book1.id, user_id: user2.id, comment: "The beginning is long", rating: 2)
 
       expected_result = 3.5
 
