d4810982ce6b6aabcb04479c22290c358fdf4fb4
ClaireBvs <clairembeauvais@gmail.com>
2018-07-15 16:37:20 -0600

    Add show page for book with user and review comment

--- spec/features/books/user_sees_show_page_spec.rb
+++ spec/features/books/user_sees_show_page_spec.rb
@@ -4,18 +4,18 @@ describe 'a user visit the show page book' do
   it 'user sees book title and review and name of user' do
       book1 = Book.create(title: "1, 2, 3")
       book2 = Book.create(title: "Brooklyn Bridge")
-      user1 = User.create(name: "Elena", review: "Fantastic", rating: 5)
-      user2 = User.create(name: "John", review: "Difficult start", rating: 2)
-      UserBook.create(book_id: book1.id, user_id: user1.id)
-      UserBook.create(book_id: book1.id, user_id: user2.id)
+      user1 = User.create(name: "Elena")
+      user2 = User.create(name: "John")
+      review1 = Review.create(book_id: book1.id, user_id: user1.id, comment: "Great book", rating: 5)
+      review2 = Review.create(book_id: book1.id, user_id: user2.id, comment: "The beginning is long", rating: 2)
 
       visit book_path(book1)
 
       expect(page).to have_content(book1.title)
-      expect(page).to have_content(user1.review)
       expect(page).to have_content(user1.name)
-      expect(page).to have_content(user2.review)
+      expect(page).to have_content(review1.comment)
       expect(page).to have_content(user2.name)
+      expect(page).to have_content(review2.comment)
     end
 
     it 'user can see average rating' do
