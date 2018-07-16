e59309f1b1a7d79363ffd3e5fad3d80a3c967199
ClaireBvs <clairembeauvais@gmail.com>
2018-07-15 18:52:24 -0600

    Add user and review for feature test index - all user stories are passing

--- spec/features/books/user_sees_all_books_spec.rb
+++ spec/features/books/user_sees_all_books_spec.rb
@@ -4,6 +4,10 @@ describe 'a user visit the index page' do
   it 'user sees all books and titles' do
       book1 = Book.create(title: "1, 2, 3")
       book2 = Book.create(title: "Brooklyn Bridge")
+      user1 = User.create(name: "Elena")
+      user2 = User.create(name: "John")
+      review1 = Review.create(book_id: book1.id, user_id: user1.id, comment: "Great book", rating: 5)
+      review2 = Review.create(book_id: book1.id, user_id: user2.id, comment: "The beginning is long", rating: 2)
 
       visit books_path
 
@@ -14,6 +18,10 @@ describe 'a user visit the index page' do
     it 'can click on a book title and go to the book show page' do
       book1 = Book.create(title: "1, 2, 3")
       book2 = Book.create(title: "Brooklyn Bridge")
+      user1 = User.create(name: "Elena")
+      user2 = User.create(name: "John")
+      review1 = Review.create(book_id: book1.id, user_id: user1.id, comment: "Great book", rating: 5)
+      review2 = Review.create(book_id: book1.id, user_id: user2.id, comment: "The beginning is long", rating: 2)
 
       visit books_path
 
